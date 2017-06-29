62561f95f9d18f9d417665c347517aba220eedee
littleflute <13699175164@sina.cn>
2017-06-29 17:44:00 -0500

    Add files via upload

---
+++ Lights Out [2008 Bonus Tracks]/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Too Hot to Handle.mp3";		
+f[2]	= "02 Just Another Suicide.mp3";		
+f[3]	= "03 Try Me.mp3";		
+f[4]	= "04 Lights Out.mp3";		
+f[5]	= "05 Gettin' Ready.mp3";		
+f[6]	= "06 Alone Again Or.mp3";		
+f[7]	= "07 Electric Phase.mp3";		
+f[8]	= "08 Love to Love.mp3";		
+f[9]	= "09 Lights Out [_][-][Live].mp3";		
+f[10]	= "10 Gettin' Ready [_][-][Live].mp3";		
+f[11]	= "11 Love to Love [_][-][Live].mp3";		
+f[12]	= "12 Try Me [_][-][Live].mp3";		
